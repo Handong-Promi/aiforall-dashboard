92b4f842f010cc13a2e8e2a144c39b1ca13c4e88
Handong-Promi <21900543@handong.edu>
2020-08-22 23:32:37 +0900

    Updates

---
+++ precache-manifest.6b20174ef1e7e2651b53e39ad2b30c11.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1bfbd22e9ab5214dc3715a33d1b82269",
+    "url": "/aiforall-dashboard/index.html"
+  },
+  {
+    "revision": "19a9dfaca4108ac60b77",
+    "url": "/aiforall-dashboard/static/css/2.de424728.chunk.css"
+  },
+  {
+    "revision": "724edef59c711e1336eb",
+    "url": "/aiforall-dashboard/static/css/main.12cf8338.chunk.css"
+  },
+  {
+    "revision": "19a9dfaca4108ac60b77",
+    "url": "/aiforall-dashboard/static/js/2.45b6fb5c.chunk.js"
+  },
+  {
+    "revision": "6e71b6ee5ea7775d33431df95d229f09",
+    "url": "/aiforall-dashboard/static/js/2.45b6fb5c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "724edef59c711e1336eb",
+    "url": "/aiforall-dashboard/static/js/main.8e5f8239.chunk.js"
+  },
+  {
+    "revision": "05a17ac3ec010db1661f",
+    "url": "/aiforall-dashboard/static/js/runtime-main.8f353ad7.js"
+  }
+]);
