8b6902ecbc82e45fdcb1f6808a69477528baa148
Handong-Promi <21900543@handong.edu>
2020-08-29 17:38:46 +0900

    Updates

--- precache-manifest.f69defa10ecd59ae6476f54c28768bc4.js
+++ precache-manifest.f69defa10ecd59ae6476f54c28768bc4.js
@@ -1,6 +1,6 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "92b3d125502650dee9b1009aa36a423a",
+    "revision": "1e2c480103a9233414f0ff30854be3f9",
     "url": "/aiforall-dashboard/index.html"
   },
   {
@@ -8,8 +8,8 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "/aiforall-dashboard/static/css/2.de424728.chunk.css"
   },
   {
-    "revision": "fce03db0fb989ccaee90",
-    "url": "/aiforall-dashboard/static/css/main.12cf8338.chunk.css"
+    "revision": "5c543a840003da9d2dc2",
+    "url": "/aiforall-dashboard/static/css/main.8e2518ed.chunk.css"
   },
   {
     "revision": "19a9dfaca4108ac60b77",
@@ -20,8 +20,8 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "/aiforall-dashboard/static/js/2.45b6fb5c.chunk.js.LICENSE.txt"
   },
   {
-    "revision": "fce03db0fb989ccaee90",
-    "url": "/aiforall-dashboard/static/js/main.4ba942a5.chunk.js"
+    "revision": "5c543a840003da9d2dc2",
+    "url": "/aiforall-dashboard/static/js/main.b6b91788.chunk.js"
   },
   {
     "revision": "05a17ac3ec010db1661f",
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/aiforall-dashboard/precache-manifest.e30cfaf20763a7163299bdb948c17e65.js"
+  "/aiforall-dashboard/precache-manifest.f69defa10ecd59ae6476f54c28768bc4.js"
 );
 
 self.addEventListener('message', (event) => {
